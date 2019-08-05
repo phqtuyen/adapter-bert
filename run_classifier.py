ccd57a493e47043594a09994d17352fb2064b680
thomas pham <thomas.pham@oracle.com>
2019-08-05 17:28:16 +1000

    minor

--- run_classifier.py
+++ run_classifier.py
@@ -902,7 +902,7 @@ def main(_):
   if FLAGS.do_train:
     train_file = os.path.join(FLAGS.output_dir, "train.tf_record")
     file_based_convert_examples_to_features(
-        train_examples, label_list, FLAGS.max_seq_length, tokenizer, train_file)
+        train_examples, all_labels, FLAGS.max_seq_length, tokenizer, train_file)
     tf.logging.info("***** Running training *****")
     tf.logging.info("  Num examples = %d", len(train_examples))
     tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
